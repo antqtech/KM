4a98e284caf988dc716bd43dce2b8437ba6bd349
LWY924 <106640769+LWY924@users.noreply.github.com>
2023-08-18 10:29:53 +0800

    指令

---
+++ ASP.NET/ASP.NET套件/usp_Sel_SPlayer1.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:	Carol
+-- Create date: 2023/08/01
+-- Description:	查詢選手資料
+-- =============================================
+CREATE Procedure usp_Sel_SPlayer1
+
+	(
+          @PID varchar(50)
+	)
+
+AS
+SELECT PID, PName_Zh, PName_En from Player WHERE PID = @PID or PPhone = @PID
+GO
+/*
+GRANT EXEC ON Stored_Procedure_Name TO PUBLIC
+
+GO
+*/
